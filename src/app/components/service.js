06c2a567f02081f72810c8b482a3685899b9bc6b
Abdur Rehman <johnshah@Johns-MacBook-Air.local>
2025-10-25 13:31:36 +0500

    Initial commit

---
+++ src/app/components/service.js
@@ -0,0 +1,47 @@
+import { FaPencilAlt, FaFirstAid } from 'react-icons/fa'
+import { FaGears } from 'react-icons/fa6'
+
+function Service() {
+  return (
+    <div>
+        <div className='text-white mt-8 mx-20'>
+          <h1 className='text-xl font-bold'>Services I Offer</h1>
+          <p>Legendary assasin John Wick (Keanu Reeves) retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seek vengence. Meanwhile Iosef's father (Michael Nyqvist) -- John's former colleague -- puts a huge bounty on John's head.</p>
+        </div>
+        <div className='mt-8 mx-20 flex gap-4'>
+          <div className='bg-white rounded-xl py-5'>
+            <FaPencilAlt className='m-auto text-4xl' />
+            <h1 className='mt-5 text-center'>Dummy Text</h1>
+            <p className='mt-3 mx-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delenti suscipit iure corporis temporibus ipsa exercitationem nesciunt impedit modi, est quae harum eaque cumque provident sit maiores enim corrupti aspernatur dolores.</p>
+            <h1 className='mt-3 text-center'>Dummy Text</h1>
+          </div>
+          <div className='bg-white rounded-xl py-5'>
+            <FaFirstAid className='m-auto text-4xl' />
+            <h1 className='mt-5 text-center'>Dummy Text</h1>
+            <p className='mt-3 mx-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delenti suscipit iure corporis temporibus ipsa exercitationem nesciunt impedit modi, est quae harum eaque cumque provident sit maiores enim corrupti aspernatur dolores.</p>
+            <h1 className='mt-3 text-center'>Dummy Text</h1>
+          </div>
+          <div className='bg-white rounded-xl py-5'>
+            <FaGears className='m-auto text-4xl' />
+            <h1 className='mt-5 text-center'>Dummy Text</h1>
+            <p className='mt-3 mx-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delenti suscipit iure corporis temporibus ipsa exercitationem nesciunt impedit modi, est quae harum eaque cumque provident sit maiores enim corrupti aspernatur dolores.</p>
+            <h1 className='mt-3 text-center'>Dummy Text</h1>
+          </div>
+        </div>
+        <div className='text-white mt-8 mx-20'>
+          <h1 className='text-xl font-bold'>Services I Offer</h1>
+          <p>Legendary assasin John Wick (Keanu Reeves) retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seek vengence. Meanwhile Iosef's father (Michael Nyqvist) -- John's former colleague -- puts a huge bounty on John's head.</p>
+        </div>
+        <div className='grid grid-cols-2 gap-3 mx-20 mt-2'>
+          <img src='image1.jpg' className='h-[400px] w-[100%]'/>
+          <img src='image2.jpg' className='h-[400px] w-[100%]'/>
+          <img src='image3.jpg' className='h-[400px] w-[100%]'/>
+          <img src='image4.jpg' className='h-[400px] w-[100%]'/>
+          <img src='image5.jpg' className='h-[400px] w-[100%]'/>
+          <img src='image6.jpg' className='h-[400px] w-[100%]'/>
+        </div>
+    </div>
+  )
+}
+
+export default Service
