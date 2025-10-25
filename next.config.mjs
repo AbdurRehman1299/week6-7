3ef827998bf595d32e391715285d8c93fa36af33
Abdur Rehman <johnshah@Johns-MacBook-Air.local>
2025-10-25 15:15:38 +0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/week—6',
+    assetPrefix: '/week—6',
     images: {
       unoptimized: true
     },
